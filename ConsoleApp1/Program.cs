ea7842ed64b77daa550e69519d007e656293488a
stas115gr <stashauzkin10@gmail.com>
2024-11-18 15:22:45 +0200

    task3

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("a=");
+            int a = int.Parse(Console.ReadLine());
+            double b = Func(a);
+            Console.WriteLine($"b={b}");
+            Console.ReadKey();
+        }
+        public static double Func(double a)
+        {
+            return 12 * Math.Pow(a, 2) + 7 * a - 6;
+        }
+    }
+}
