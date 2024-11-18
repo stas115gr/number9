29137311c4e4f59048ac9dc5535cfea875e77803
stas115gr <stashauzkin10@gmail.com>
2024-11-18 15:19:27 +0200

    task2

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("a=");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("b=");
+            int b = int.Parse(Console.ReadLine());
+            int result = Sum(a, b);
+            Console.Write($"Сумма = {result}");
+            Console.ReadKey();
+        }
+        public static int Sum(int a, int b)
+        {
+            return a + b;
+
+        }
+    }
+}
