788df4aa04cd54850c76bf700bf9c23e56fcb36e
stas115gr <stashauzkin10@gmail.com>
2024-11-18 15:14:43 +0200

    task1

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SayHello();
+            SayBye();
+            Console.ReadKey();
+        }
+        public static void SayHello()
+        {
+            Console.Write("Привет");
+            Console.Read();
+        }
+        public static void SayBye()
+        {
+            Console.WriteLine("Пока");
+        }
+    }
+}
